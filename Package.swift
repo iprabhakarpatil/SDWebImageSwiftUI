7ba49623ef8e119e1c6dea0e9e435ba93a3a0d69
Prabhakar Patil <12987010+iprabhakarpatil@users.noreply.github.com>
2025-07-16 15:27:10 +0530

    Update Package.swift
    removed the dependency of SDWebImage for managing the package graph

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", "5.21.0"..<"5.21.1")
+        // .package(url: "https://github.com/SDWebImage/SDWebImage.git", "5.21.0"..<"5.21.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SDWebImageSwiftUI",
-            dependencies: ["SDWebImage"],
+            // dependencies: ["SDWebImage"],
             path: "SDWebImageSwiftUI",
             sources: ["Classes"],
             resources: [.copy("Resources/PrivacyInfo.xcprivacy")]
